b34da4a218ead5ee19154115a3c75b106e381dae
Upian <terry0729@naver.com>
2025-04-21 17:07:39 +0900

    change style
    중괄호 변경

--- ServerEngine/ClientTest/main.cpp
+++ ServerEngine/ClientTest/main.cpp
@@ -3,26 +3,28 @@
 #include "TCPClientModule.h"
 #include "Proxy.h"
 
-int main(void) {
+int main(void)
+{
 	TCPClientModule client;
 	std::string ip = "127.0.0.1";
 	client.ConnectServer(ip, 25000, 1);
-	
+
 	char line[kMAX_BUFFER_SIZE + 1];
-	while (std::cin.getline(line, kMAX_BUFFER_SIZE + 1)) {
+	while (std::cin.getline(line, kMAX_BUFFER_SIZE + 1))
+	{
 		Buffer msg;
-//		msg.Clear();
+		//		msg.Clear();
 		msg.SetBufferSize(std::strlen(line));
 		std::memcpy(msg.GetBody(), line, msg.GetBodySize());
 		msg.EncodeHeader();
 		client.Send(1, msg);
 	}
-//	asio::io_context* io_context = new asio::io_context;
-//	tcp::resolver r(*io_context);
-//	auto proxy = Proxy::Create(io_context);
-//	
-//	proxy->Start(r.resolve("127.0.0.1", "25000"));
-//
-//	io_context->run();
+	//	asio::io_context* io_context = new asio::io_context;
+	//	tcp::resolver r(*io_context);
+	//	auto proxy = Proxy::Create(io_context);
+	//	
+	//	proxy->Start(r.resolve("127.0.0.1", "25000"));
+	//
+	//	io_context->run();
 	getchar();
 }
